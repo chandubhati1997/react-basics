c761b6b1b1cfa507823ec1b81865eb7781400e9e
chandubhati1997 <chandreveersingh@gmail.com>
2025-03-12 14:16:42 +0530

    react-basics

---
+++ App.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+const heading=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{},"hello there"),React.createElement("h2",{},"hello there")]));
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+
+
+console.log(heading);
+
+
+root.render(heading);
+
+// root.render()
+
+
+
